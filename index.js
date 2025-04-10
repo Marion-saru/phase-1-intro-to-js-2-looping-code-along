882eb5694345a1de6dbe907d31be43a22d835902
Marion Saru <maghangasaru@gmail.com>
2025-04-10 21:15:39 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names =  [ 'Guadalupe', 'Ollie', 'Aki' ];
+
+function writeCards(names) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return messages;
+  }
+  console.log(writeCards(names));
+
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  console.log(countDown(10));
